236f235ddbd8ada79eb35652d1b9d75d86d3eedd
Omar Abdelwahab <omara@fb.com>
2025-10-06 13:30:58 -0700

    removed a single file

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the terms described in the LICENSE file in
-# the root directory of this source tree.
-
-# Global pytest plugins configuration
-pytest_plugins = ["tests.integration.fixtures.common"]
