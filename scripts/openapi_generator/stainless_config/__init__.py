a9578a3810fafcd551a6cb17260e92a82c3198b0
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2025-11-14 20:08:02 -0800

    precommit

--- scripts/openapi_generator/stainless_config/__init__.py
+++ scripts/openapi_generator/stainless_config/__init__.py
@@ -1,2 +1,7 @@
-# Package marker for Stainless config generation.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
 
+# Package marker for Stainless config generation.
