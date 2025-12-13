e099f7012f4d4ce613c094dc34dac2909fe168dc
Jaideep Rao <jrao@redhat.com>
2025-12-13 11:23:12 +0530

    make mcp import lazy
    Signed-off-by: Jaideep Rao <jrao@redhat.com>

--- src/llama_stack_api/__init__.py
+++ src/llama_stack_api/__init__.py
@@ -70,7 +70,6 @@ from .common.content_types import (
 )
 from .common.errors import (
     ConflictError,
-    ConnectorAlreadyRegisteredError,
     ConnectorNotFoundError,
     ConnectorToolNotFoundError,
     DatasetNotFoundError,
@@ -576,7 +575,6 @@ __all__ = [
     "CompletionInputType",
     "CompletionRequest",
     "Connector",
-    "ConnectorAlreadyRegisteredError",
     "ConnectorNotFoundError",
     "ConnectorToolNotFoundError",
     "ConnectorInput",
