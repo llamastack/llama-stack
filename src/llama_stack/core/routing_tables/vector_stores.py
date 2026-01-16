17ffa91732c8d437f4a9391ac7a86562de9a6c5f
Francisco Javier Arceo <farceo@redhat.com>
2026-01-16 09:39:39 -0500

    fix
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- src/llama_stack/core/routing_tables/vector_stores.py
+++ src/llama_stack/core/routing_tables/vector_stores.py
@@ -12,9 +12,9 @@ from llama_stack.core.datatypes import (
 from llama_stack.log import get_logger
 
 # Removed VectorStores import to avoid exposing public API
+from llama_stack.providers.utils.vector_io.filters import Filter
 from llama_stack_api import (
     EmbeddedChunk,
-    Filter,
     InterleavedContent,
     ModelNotFoundError,
     ModelType,
