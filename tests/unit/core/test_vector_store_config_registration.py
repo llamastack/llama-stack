0b0a22429a3a65f4e3b05000ce86b0c2e2893f2c
Sébastien Han <seb@redhat.com>
2026-01-19 16:58:29 +0100

    Update test_vector_store_config_registration.py
    fix imports

--- tests/unit/core/test_vector_store_config_registration.py
+++ tests/unit/core/test_vector_store_config_registration.py
@@ -9,15 +9,14 @@
 import json
 from unittest.mock import AsyncMock
 
-from llama_stack_api import Api, Model, ModelType, VectorStore
-from llama_stack_api.vector_stores import VectorStoreInput
-
+from llama_stack.apis.model import Model, ModelType
+from llama_stack.apis.vector_stores import VectorStore, VectorStoreInput
 from llama_stack.core.datatypes import RegisteredResources, StackConfig, VectorStoresConfig
 from llama_stack.core.routing_tables.vector_stores import VectorStoresRoutingTable
 from llama_stack.core.stack import register_resources
 from llama_stack.core.storage.datatypes import ServerStoresConfig, StorageConfig
+from llama_stack.providers.datatypes import Api
 from llama_stack.providers.utils.memory.openai_vector_store_mixin import OpenAIVectorStoreMixin
-
 
 class TestVectorStoreRegistration:
     """Test vector store registration from configuration."""
